acef4f0ea9c61763fedfbf7feb1335e89ab22c7c
Sally MacNicholas <smacnicholas@gmail.com>
2015-06-03 22:28:28 -0600

    finished tagging feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'articles#index'
+  # root to: 'articles#index' - why is this needed!!?!?
   resources :articles do
     resources :comments
   end
+  resources :tags
 end
